de963b7c95780f03fd7cb334f4966d3ac57e6d0f
kriskatacom <info@kriskata.com>
2026-05-13 16:29:08 +0300

    feat(database): integrate Eloquent ORM and create Plugin model
    - Set up Eloquent ORM in index.php for database connection using environment variables.
    - Add Plugin model with fillable attributes and isActive method.
    - Create migration for plugins table with necessary columns and constraints.
    - Add Phinx configuration file for database migrations.
    - Update package.json with migration scripts for easier database management.

--- index.php
+++ index.php
@@ -8,9 +8,26 @@ use Flex\Core\Database;
 use Flex\Core\Events\EventManager;
 use Flex\Core\Plugins\PluginManager;
 use Flex\Core\Routing\Router;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => $_ENV['DB_HOST'],
+    'database'  => $_ENV['DB_NAME'],
+    'username'  => $_ENV['DB_USER'],
+    'password'  => $_ENV['DB_PASS'],
+    'charset'   => $_ENV['DB_CHAR'],
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix'    => '',
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 function db() {
     return Database::getInstance();
